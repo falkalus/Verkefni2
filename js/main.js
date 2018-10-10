b2b818d93d63d28ec5e7feeea6fb866eac9562b4
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-10 16:02:01 +0000

    laga
    laga

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
-/* -------------Events 2------------- */
+/* -------------Events 0 ------------ */
 var mouse = document.querySelector("#mouse");
 console.log(mouse.innerHTML);
-
 
 mouse.onmouseover = function(e){
 	mouse.style.backgroundImage = "url(img/mouse.png)";
 	mouse.style.backgroundSize = "100px 100px";
 	mouse.style.backgroundRepeat = "no-repeat";
-	mouse.style.height = "200px";
+	mouse.style.height = "120px";
 	mouse.style.width = "200px";
+	mouse.style.paddingLeft = "100px";
 }
 mouse.onmouseleave = function(e){
 	mouse.style.backgroundImage = "";
 }
 bubble.onclick = function(e){
-	bubble.style.backgroundImage = "url(img/bubble.png)";
+	bubble.style.backgroundImage = "url(img/tist.png)";
 	bubble.style.backgroundSize = "100px 100px";
 	bubble.style.backgroundRepeat = "no-repeat";
-	bubble.style.height = "200px";
+	bubble.style.height = "100px";
 	bubble.style.width = "200px";
+	bubble.style.paddingLeft = "100px";
 }
 
+/* -------------ostur í mark------------- */
+var ostur = document.querySelector("#ostur");
+	ostur.style.backgroundImage = "url(img/ostur.png)";
+	ostur.style.backgroundSize = "100px 100px";
+	ostur.style.backgroundRepeat = "no-repeat";
+	ostur.style.height = "100px";
+	ostur.style.width = "200px";
 
+/* -------------færa kassa með takka------------- */
+var secondLeft = 0;
+var goLeft = function(e){
+	console.log(e.key);
+	if(e.key==="ArrowRight"){ //nota if
+	secondLeft = secondLeft + 10;
+	ostur.style.marginLeft = secondLeft+"px";
+	}
+}
+document.onkeydown = goLeft;
 
-/* -------------Function 2------------- */
+/* -------------Function 1------------- */
+var hallo = function(nafn, aldur){
+	return "Hæ ég heiti " + nafn + " og ég er " + aldur + " ára";
+}
+var mus = hallo("Max", 10);
+console.log(mus);
+
 
 /* -------------If -Else 3------------- */
-var clickCount = 0;
-console.log(e)
+
